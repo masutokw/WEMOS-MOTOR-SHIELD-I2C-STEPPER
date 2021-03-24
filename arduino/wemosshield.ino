264f857cb4bfc6948f16c4920027aca066ab7118
masutokw <masutokw@users.noreply.github.com>
2021-03-24 12:21:20 +0100

    Write i2c float function for speed setting in msteps/s

--- arduino/wemosshield.ino
+++ arduino/wemosshield.ino
@@ -9,6 +9,15 @@
 #define MOTOR_GET_TARGET 0x05
 #define MOTOR_SET_TICKS 0x06
 #define MOTOR_SET_DIR_RES 0x7
+#define MOTOR_SET_DIR_RES 0x07
+#define MOTOR_SET_PRESCALER 0x08
+#define MOTOR_SET_WAVE_SCALE 0x09
+#define MOTOR_SET_SPEED 0x0A
+#define MOTOR_SET_TARGET_SPEED 0x0B
+#define MOTOR_GET_TARGET_SPEED 0x0C
+#define MOTOR_GET_SPEED 0x0D
+#define MOTOR_GET_DIR_RES 0x0e
+#define PRINT_WAVE 0x44
 void setup() {
  Wire.begin();        // join i2c bus (address optional for master)
   Serial.begin(9600);  // start serial for output
@@ -46,15 +55,27 @@ void write_wms( uint8_t command, int address,int32_t value)
   Wire.endTransmission();    
 
 }
+void write_wmsf( uint8_t command, int address,float value)
+{ 
+  char *pointer;
+  pointer=(char*)(&value);
+ Wire.beginTransmission(address); 
+  Wire.write( command);
+  Wire.write(pointer,4);
+  Wire.endTransmission();    
+
+}
 void loop() {
 
  //goto from 0 to 1000 
  int32_t pos=0;//set motor counter to 0
- int32_t target= 1000;//set desired position 1000
- int32_t speed=3000;//20,8 us * 300 =0.624s  ->16,02 microsteps/s 
+ int32_t target= 10000;//set desired position 10000
+ int32_t speed=300;//20,8 us * 300 =0.624s  ->16,02 microsteps/s
+ float speedf=2000.0;//2000 msteps/s 
  write_wms(MOTOR_SET_COUNT,SLAVE_ADDRESS_1,pos);//send counter
  write_wms(MOTOR_SET_TARGET,SLAVE_ADDRESS_1,target);//target set
- write_wms(MOTOR_SET_TICKS,SLAVE_ADDRESS_1,speed);//command speed
+// write_wms(MOTOR_SET_TICKS,SLAVE_ADDRESS_1,speed);//command speed
+ write_wmsf(MOTOR_SET_SPEED ,SLAVE_ADDRESS_1,speedf);//command speed
  while ( (pos=read_wms( MOTOR_GET_COUNT ,SLAVE_ADDRESS_1))!=target)
  {
   delay (100);
@@ -64,11 +85,12 @@ void loop() {
 
 //now we command back 0 pos
   target= 0;//set desired position 1000
- speed=-3000;//20,8 us * 300 =0.624s  ->-16,02 microsteps/s 
+ speed=-2000;//20,8 us * 300 =0.624s  ->-16,02 microsteps/s 
  int32_t resolution=2; //motor will no run on 16 msteps 
  write_wms(MOTOR_SET_DIR_RES,SLAVE_ADDRESS_1,resolution);
  write_wms(MOTOR_SET_TARGET,SLAVE_ADDRESS_1,target);//target set
- write_wms(MOTOR_SET_TICKS,SLAVE_ADDRESS_1,speed);//command speed
+// write_wms(MOTOR_SET_TICKS,SLAVE_ADDRESS_1,speed);//command speed
+ write_wmsf(MOTOR_SET_SPEED ,SLAVE_ADDRESS_1,speedf);//command speed
  while ( (pos=read_wms( MOTOR_GET_COUNT ,SLAVE_ADDRESS_1))!=target)
  {
   delay (100);
